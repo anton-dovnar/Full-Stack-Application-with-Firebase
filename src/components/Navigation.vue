2acff4ecd914a557ee9e065adc11c39bb6f7622b
Anton Dovnar <fode4cun@gmail.com>
2021-07-26 18:10:29 +0300

    Add Navigation Component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meeting Log</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/meetings">Meetings</router-link>
+          <router-link class="nav-item nav-link" to="/login">Log In</router-link>
+          <router-link class="nav-item nav-link" to="/register">Register</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
